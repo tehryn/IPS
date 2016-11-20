f1f9098b62cf1f3f377e0080856fcf8dce9e0e14
tehryn <thetehryn@gmail.com>
2016-11-20 21:50:29 +0100

    This might work...

--- du2/pgrep.cpp
+++ du2/pgrep.cpp
@@ -12,10 +12,13 @@
 char *line;
 
 /** @var SCORE variable for storing score of line */
-int SCORE = 0;
+int score = 0;
 
-/** @var mutex Mutex used to protect data race of @var SCORE*/
-std::mutex mutex;
+/** @var mutex vector of mutexes that provide datarace*/
+std::vector<std::mutex *> mutex;
+
+/** @var variable for comunication between main process and threads */
+int res;
 
 /** Function for parsing arguments - retrivieving score values
 @param argc Number of arguments
@@ -46,13 +49,7 @@ char *read_line(int *res);
 @pre idx < length of score
 @pre idx < length of regs
 */
-void calc_score (unsigned idx, int score[], char* regs[]) {
-	if (std::regex_match(line, std::regex(regs[idx]))) {
-		mutex.lock(); // locking critical setion
-		SCORE += score[idx]; // reading and overwriting value of SCORE
-		mutex.unlock(); // unlocking critical section
-	}
-}
+int calc_score (unsigned idx, int *scores, char **regs);
 
 int main(int argc, char* argv[]){
 
@@ -62,52 +59,60 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 
-	int* score = parse_score(argc, argv); // array of scores
-	if (score == NULL) {
+	int* scores = parse_score(argc, argv); // array of scores
+	if (scores == NULL) {
 		fprintf(stderr, "Memory allocation failed\n");
 		return -1;
 	}
 
 	char** regs = parse_reg(argc, argv); // array of regular expresions
 	if (regs == NULL) {
-		free(score);
+		free(scores);
 		fprintf(stderr, "Memory allocation failed\n");
 		return -1;
 	}
 	std::vector <std::thread *> threads; // vector of threads
 	unsigned threads_number = (argc-1)/2; // number of needed threads
 	int min_score = atoi(argv[1]); // retrieve min score from arguments
-	int res; // needed for char* read_line(int *res);
 
-	threads.resize(threads_number); // resize vector of threads
-
-	/* Reading lines */
-	while ((line = read_line(&res)) != NULL) {
-		/* Starting threads */
-		for(unsigned i = 0; i < threads_number; i++) {
-			std::thread *new_thread = new std::thread (calc_score, i, score, regs);
-			threads[i] = new_thread;
-//			printf("Score: %i, regex: %s\n", score[i], regs[i]);
-		}
-
-		/* Waiting for threads and calling destructor */
-		for (unsigned i = 0; i < threads_number; i++){
-			(*(threads[i])).join();
-			delete threads[i];
-		}
-
-		/* Should we print line number? */
-		if (SCORE >= min_score) {
-			printf("%s\n", line);
-		}
-
-		SCORE = 0; // reseting score value
-
-		free(line); // freeing current line, we won't need it anymore
+	mutex.resize(threads_number+1); // resize vector of mutexes
+	for(unsigned i = 0; i <= threads_number; i++){
+		std::mutex *new_mutex = new std::mutex();
+		mutex[i] = new_mutex;
+		(*(mutex[i])).lock();
 	}
 
+	threads.resize(threads_number); // resize vector of threads
+	/* Starting threads */
+	for(unsigned i = 0; i < threads_number; i++) {
+		std::thread *new_thread = new std::thread (calc_score, i, scores, regs);
+		threads[i] = new_thread;
+	}
+
+	/* Reading lines */
+	res = 1;
+	while (res) {
+		line = read_line(&res);
+		if (res) {
+//			printf ("ODEMIKAM 0\n");
+			(*(mutex[0])).unlock(); // release first thread
+			(*(mutex[threads_number])).lock(); // wait until all threads finish their work
+			/* Should we print line number? */
+			if (score >= min_score && res) {
+				printf("%s\n", line);
+			}
+			score = 0; // reseting score value
+			free(line); // freeing current line, we won't need it anymore
+		}
+	}
+//	printf ("ODEMIKAM 0 - koncime\n");
+	(*(mutex[0])).unlock();
 	/* free memory and end function */
-	free(score);
+	for (unsigned i = 0; i < threads_number; i++){
+		(*(threads[i])).join();
+		delete threads[i];
+	}
+	free(scores);
 	free(regs);
 	return 0;
 }
@@ -173,10 +178,20 @@ char *read_line(int *res) {
 }
 
 /* Function for threads - calculating score */
-void calc_score (const int id, const int *score, char** regs) { // TODO why regs cant be const char?
-	if (std::regex_match(line, std::regex(regs[id]))) {
-		mutex.lock(); // locking critical setion
-		SCORE += score[id]; // reading and overwriting value of SCORE
-		mutex.unlock(); // unlocking critical section
+/* Function for threads - calculating score */
+int calc_score(unsigned idx, int *scores, char **regs) {
+//	printf("%u STARTED\n", idx);
+	(*(mutex[idx])).lock();
+//	printf("%u UNLOCKED\n", idx);
+	while(res) {
+		if (std::regex_match(line, std::regex(regs[idx]))) {
+			score += scores[idx]; // reading and overwriting value of SCORE
+		}
+//		printf("%u LOCKED, ODEMIKAM %u\n", idx, idx+1);
+		(*(mutex[idx+1])).unlock();
+		(*(mutex[idx])).lock();
 	}
+//	printf("%u KONCIM\n", idx);
+	(*(mutex[idx+1])).unlock();
+	return idx;
 }
